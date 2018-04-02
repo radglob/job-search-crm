6ee0b8c79ea40673be124c44a9b8f31a1b35ab4a
Jeremy Neal <jneal1992@gmail.com>
2018-04-02 14:17:14 -0400

    Created company, position models.

--- job_search_crm/applications/models.py
+++ job_search_crm/applications/models.py
@@ -9,3 +9,24 @@ class CustomerProfile(models.Model):
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=50, blank=True)
     birth_date = models.DateField(null=True, blank=True)
+
+
+class Company(models.Model):
+    company_name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    sub_industry = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.company_name
+
+
+class Position(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    position_name = models.CharField(max_length=50)
+    is_remote = models.BooleanField()
+    min_salary = models.IntegerField()
+    max_salary = models.IntegerField()
+    tech_stack = models.TextField(max_length=500, blank=True, null=True)
+
+    def __str__(self):
+        return " at ".join((self.position_name, self.company.company_name))
